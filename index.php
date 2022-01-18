9d9f7494ab3365f94249a973c90b53c556262a83
celebrater <tkgm45nbchk@gmail.com>
2022-01-18 12:56:37 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$test_url = 1;
+$max_thumb = 16;
+
+require_once("info.php");
+
+$count = count($images_array);
+
+function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="Author" content="Enin Fujimi">
+    <title>Novel Server 2</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/novel.css">
+    <link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+    <h1 class="gallery">
+        <a href="/">
+            Novel Server 2
+        </a>
+    </h1>
+    <div class="container">
+        <?php for ($i = 0; $i < $count; $i++) : ?>
+            <div class="thumb_box">
+                <a href="view.php?id=<?php echo h($i); ?>">
+                    <img src="<?php echo "img/".$images_array[$i]["src"]; ?>">
+                </a>
+            </div>
+        <?php endfor; ?>
+    </div>
+
+</body>
+</html>
