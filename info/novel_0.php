4007c560be6b1824a322b3982648f5df4fce3854
celebrater <tkgm45nbchk@gmail.com>
2022-04-03 04:33:30 +0900

    moved php_hp_bbs_2 and fp_common_modules into fujimipolis temporary

---
+++ info/novel_0.php
@@ -0,0 +1,9 @@
+<?php
+
+// （例）第二章十三話 => ["episode" => 2, "chapter" => 13
+$index = [
+    ["episode" => 1, "chapter" => 1, "subtitle" => "第一話"],
+    ["episode" => 1, "chapter" => 2, "subtitle" => "第二話"],
+    ["episode" => 1, "chapter" => 3, "subtitle" => "第三話"],
+    ["episode" => 1, "chapter" => 4, "subtitle" => "第四話"]
+];
---
+++ list.php
@@ -0,0 +1,8 @@
+<?php
+
+$title_list = [
+    ["id" => 2, "title" => "白金記 - Unify the World"],
+    ["id" => 1, "title" => "極楽戦争 - End of End"],
+    ["id" => 0, "title" => "神仏非習合 - God vs Buddha"]
+];
+
